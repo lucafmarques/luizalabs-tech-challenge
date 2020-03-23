e9f5c6b858bdc6bceae4e00a6ba4f152ce3740b0
lucafmarques <lucafmarqs@gmail.com>
2020-03-23 11:26:48 -0300

    Adds get method to fav route, allowing for retrieval of user favorites.

--- app/routers/favorites.py
+++ app/routers/favorites.py
@@ -10,7 +10,7 @@ router = APIRouter()
 @router.post("/{user_email}/{product_id}")
 async def add_favorite(user_email: str, product_id: str, db: Session = Depends(db_session)):
     db_user = get_user_by_email(db, user_email)
-    if not db_user:
+    if db_user is None:
         return JSONResponse({
             "message": "User doesn't exist"
         }, status_code=status.HTTP_400_BAD_REQUEST)
@@ -27,4 +27,19 @@ async def add_favorite(user_email: str, product_id: str, db: Session = Depends(d
             "message": "Operation failed"
         }, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    return user.json()
+    return JSONResponse({
+        "message": "Added item to user favorites."
+    }, status_code=status.HTTP_200_OK)
+
+@router.get("/{user_email}")
+async def get_favorites(user_email: str, db: Session = Depends(db_session)):
+    db_user = get_user_by_email(db, user_email)
+    if db_user is None:
+        return JSONResponse({
+            "message": "User doesn't exist"
+        }, status_code=status.HTTP_400_BAD_REQUEST)
+
+    return JSONResponse({
+        "uuid": str(db_user.uuid),
+        "favorites": db_user.favorites
+    }, status_code=status.HTTP_200_OK)
