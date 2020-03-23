74f7b1c159e549dae45f6cb64a15753b01bf994e
lucafmarques <lucafmarqs@gmail.com>
2020-03-22 23:04:10 -0300

    Adds 'admin' to config, reading from a config-file.

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
+import os
 import json
+
 from pydantic import BaseModel
+
+class AuthUser(BaseModel):
+    username: str
+    password: str
 
 class Config(BaseModel):
     DB_URL: str = "127.0.0.1:5432"
     DB_USER: str = "admin"
     DB_PASSWORD: str = "7eef259c-e762-416d-ae00-dee029ab6d9b"
+    ADMIN: AuthUser
+    SECRET_KEY: str = "b2lhZ2FsZXJhcXVldGFtZWF2YWxpYW5kbw=="
     PRODUCT_URL: str = "http://challenge-api.luizalabs.com/api/product/"
 
     def URL(self):
@@ -16,3 +24,5 @@ def read_config(path: str):
         data = json.load(raw_config)
 
     return Config.parse_obj(data)
+
+config = config = read_config(os.getenv('CONFIG_PATH'))
